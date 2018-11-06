906cc24acf747b02592c43af45a395a8a59f430c
mvillanueva <Maximiliano.Villanueva@e-campus.uab.cat>
2018-11-06 13:01:45 +0100

    changes

---
+++ question.py
@@ -0,0 +1,19 @@
+class Question:
+
+    def __init__(self, column, value):
+        self.column = column
+        self.value = value
+
+    def match(self, example):
+        val = example[self.column]
+        if is_numeric(val):
+            return val >= self.value
+        else:
+            return val == self.value
+
+    def __repr__(self):
+        condition = "=="
+        if is_numeric(self.value):
+            condition = ">="
+        return "Is %s %s %s?" % (
+            header[self.column], condition, str(self.value))
