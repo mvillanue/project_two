489ac143d6b01edc7e1599e3d5f616ee2ec7e43c
mvillanueva <Maximiliano.Villanueva@e-campus.uab.cat>
2018-11-06 12:32:12 +0100

    utilities

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-
-def headers():
-    return ["cap-shape","cap-surface","cap-color","bruises","odor","gil-attachment","gill-spacing","gill-size","gill-color","salk-shape","stalk-root","stalk-surface-above-ring","stalk-surface-below-ring","stalk-color-above-ring","stalk-color-below-ring","veil-type","veil-color","ring-number","ring-type","spore-print-color","population","habitat"]
-
-def readFile(filename):
-    data = list()
-    with open(filename) as f:
-        for line in f:
-            data.append(line.rstrip())
-    
-    return data
+from utilities import *
+from pptree import *
 
 print readFile("expanded")
+print headers()
 
 
-from pptree import *
+
+
+
+
+
 
 """shame = Node("shame")
 
---
+++ utilities.py
@@ -0,0 +1,10 @@
+def headers():
+    return ["cap-shape","cap-surface","cap-color","bruises","odor","gil-attachment","gill-spacing","gill-size","gill-color","salk-shape","stalk-root","stalk-surface-above-ring","stalk-surface-below-ring","stalk-color-above-ring","stalk-color-below-ring","veil-type","veil-color","ring-number","ring-type","spore-print-color","population","habitat"]
+
+def readFile(filename):
+    data = list()
+    with open(filename) as f:
+        for line in f:
+            data.append(line.rstrip())
+    
+    return data
