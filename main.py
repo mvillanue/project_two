31d66813259328b894f87fde8428c7f01a93924f
mvillanueva <Maximiliano.Villanueva@e-campus.uab.cat>
2018-11-06 12:01:46 +0100

    headers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
-
+def headers():
+    return ["cap-shape","cap-surface","cap-color","bruises","odor","gil-attachment","gill-spacing","gill-size","gill-color","salk-shape","stalk-root","stalk-surface-above-ring","stalk-surface-below-ring","stalk-color-above-ring","stalk-color-below-ring","veil-type","veil-color","ring-number","ring-type","spore-print-color","population","habitat"]
 
 def readFile(filename):
     data = list()
